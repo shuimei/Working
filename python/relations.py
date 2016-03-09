158655c5708e2bb4014c99d8234370658150c876
shuimei <dengshunqiang@outlook.com>
2016-03-09 17:57:41 +0800

    relations.py modified

--- python/relations.py
+++ python/relations.py
@@ -1,59 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 26 16:26:46 2016
-
-@author: root
-"""
-import random
+from relations import *
 import itertools
+min_sup_percent=0.5
+size=len(Data)
+min_sup=min_sup_percent*size
+FreqDic = {} #{itemset1:freq1,itemsets2:freq2}
+for event in Data:
+	for item in Data[event]:
+		if item in FreqDic:
+			FreqDic[item] += 1
+		else:
+			FreqDic[item] = 1
 
-def makeRandomRecords(recNum,items):
-    records=[]
-    for rec in xrange(recNum):
-        length=random.randint(1,len(items))
-        record=[]
-        for l in xrange(length):
-            record.append(items[random.randint(0,length-1)])
-        records.append({'id':rec+1,'item':record})
-    return records
-    
-    
-def subset_1(records,items):
-    allInOne=[]
-    for rec in records:
-        allInOne.extend(rec['item'])
-    frequent_1=[]
-    for item in items:
-        frequent_1.append({'item':item,'count':allInOne.count(item)})
-    return frequent_1
-    
-def subset(items, length):
-    return list(itertools.permutations(items,length))
-    
-def frequent(records,items, itemsLength):
-    assert itemsLength > 0
-    frequents=[]
-    preFrequent=iter(subset(items,itemsLength))
-    for one in preFrequent:
-        count=0
-        for rec in records:
-            if listInList(one, rec['item']):
-                count+=1
-        frequents.append({'items':list(one), 'surpport':float(count)/len(records)})
-    return frequents
-    
+for k in FreqDic.keys():
+	if FreqDic[k]<min_sup:
+		del(FreqDic[k])
+
+possible=[]
+for i in xrange(2,len(FreqDic.keys())):
+	possible.extend(list(itertools.combinations(FreqDic.keys(),i)))
 
 def listInList(smallList,largeList):
     if map(lambda a: a in largeList, smallList).count(True) == len(smallList):
         return True
     else:
         return False
-    
-    
-    
 
-        
-        
-    
-    
-    
+FreqSets=[]
+for p in possible:
+	count=0
+	for event in Data:
+		if listInList(list(p),Data[event]):
+			count+=1
+	FreqSets.append((p,count))
+
+print [f for f in FreqSets if f[1]>min_sup]
